e485cc67ffd08811e2319bb5f77b0a49b11ca60b
Pr1me000 <overall-rug-dust@duck.com>
2026-03-03 19:55:56 +0100

    style: amélioration du design du bouton biométrique

---
+++ lib/security_service.dart
@@ -0,0 +1,6 @@
+class SecurityService {
+  static Future<void> sendEncryptedKey(String deviceId) async {
+    print("Tentative d'échange sécurisé avec : $deviceId");
+    // reste du code pour sécuriser l'échange de la clé ... @Uramix
+  }
+}
